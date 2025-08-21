c4db9c79c288a5d4d01b44bba25280c1590c9cd8
Elias Paknejad <627105+elpadev@users.noreply.github.com>
2025-08-21 16:54:20 +0000

    Reset

---
+++ src/kubebuilder/install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+echo "Activating feature 'kubebuilder'"
+echo "The provided version is: ${VERSION}"
+
+# Download kubebuilder and install it locally.
+
+# if VERSION if latest
+if [ "${VERSION}" = "latest" ]; then
+  curl -L -o kubebuilder "https://go.kubebuilder.io/dl/latest/$(go env GOOS)/$(go env GOARCH)"
+else
+  curl -L -o kubebuilder "https://github.com/kubernetes-sigs/kubebuilder/releases/download/${VERSION}/kubebuilder_$(go env GOOS)_$(go env GOARCH)"
+fi
+
+chmod +x kubebuilder && sudo mv kubebuilder /usr/local/bin/
