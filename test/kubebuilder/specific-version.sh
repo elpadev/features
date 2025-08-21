9bd4081e0417bf78df0381950ca0eb1e6b44dd74
Elias Paknejad <627105+elpadev@users.noreply.github.com>
2025-08-21 19:18:22 +0000

    Fix test script

--- test/kubebuilder/specific-version.sh
+++ test/kubebuilder/specific-version.sh
@@ -10,7 +10,7 @@ source dev-container-features-test-lib
 
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
-check "execute command" kubebuilder version | grep -q "${version#v}""
+check "execute command" kubebuilder version | grep -q "${version#v}"
 
 # Report results
 # If any of the checks above exited with a non-zero exit code, the test will fail.
